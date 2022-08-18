7eb10c85cbb532d81d70bc189d5fe967d91871e2
Destiny198523 <luanfei523@163.com>
2022-08-18 13:15:45 +0800

    第三次提交

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public string count = "这是第三次提交";
     // Start is called before the first frame update
     void Start()
     {
