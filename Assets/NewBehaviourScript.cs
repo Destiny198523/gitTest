42153f032d979b1123f881627d15d92734613cda
Destiny198523 <30925433+Destiny198523@users.noreply.github.com>
2022-08-22 11:42:23 +0800

    在github上对脚本进行更改

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public string count = "3";
+    public string count = "Editor in Github";
     // Start is called before the first frame update
     void Start()
     {
